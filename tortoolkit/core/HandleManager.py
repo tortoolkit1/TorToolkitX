b756fdf94329414f532f711112638f6cac929bb4
tortoolkit1 <94689429+tortoolkit1@users.noreply.github.com>
2021-11-20 14:14:29 +0530

    Update HandleManager.py

--- tortoolkit/core/HandleManager.py
+++ tortoolkit/core/HandleManager.py
@@ -467,7 +467,7 @@ async def set_password_zip(message):
             await message.reply(f"Cannot update the password this is not your download.")
 
 async def start_handler(event):
-    msg = "<b>Hello This is TorToolkitX an instance of</b> <a href='https://github.com/KangersHub/TorToolkitX'>This Repo</a>. <b>Try the repo for yourself and dont forget to put a STAR and fork.</b>"
+    msg = "<b>Hello I Am An TorToolKit Mod Made By</b> <a href='https://telegram.me/pokemontamilmaster'>Pokemon Master</a>. <b>You Can Use Me On https://telegram.me/joinchat/WGkWtxmQYI0zMTZl.</b>"
     await event.reply(msg, parse_mode="html")
 
 def progress_bar(percentage):
