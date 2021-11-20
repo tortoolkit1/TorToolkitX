456a7eebf2931dd225f5a5adbca73bd4b10d22fc
Abhinand Dhandapani <90457739+abhinanddhandapani@users.noreply.github.com>
2021-11-20 14:47:12 +0530

    Updated Start Msg.

--- tortoolkit/core/HandleManager.py
+++ tortoolkit/core/HandleManager.py
@@ -467,7 +467,7 @@ async def set_password_zip(message):
             await message.reply(f"Cannot update the password this is not your download.")
 
 async def start_handler(event):
-    msg = "<img src=\"https://github.com/tortoolkit1/TorToolkitX/blob/alpha/toolkit.jpg?raw=true\" alt=\"Pokemon Master\"><b>Hello I Am An TorToolKit Mod Made By <a href='https://telegram.me/pokemontamilmaster'>Pokemon Master</a>.</b> <b>You Can Use Me On <a href=\"https://telegram.me/joinchat/WGkWtxmQYI0zMTZl\">Public Torrent Leech Group</a>.</b>"
+    msg = "<b>Hello I Am An TorToolKit Mod </b> <b>Hosted on <a href=\"https://telegram.me/joinchat/WGkWtxmQYI0zMTZl\">Public Torrent Leech Group</a>. Created By <a href='https://telegram.me/pokemontamilmaster'>Pokemon Master</a></b>"
     await event.reply(msg, parse_mode="html")
 
 def progress_bar(percentage):
